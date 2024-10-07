66d070145fcda7f9d2fde5656b25d22eccc18947
Kumail Abbas <kkumailabbas142@gmail.com>
2024-10-07 15:26:02 +0500

    first react and vite project setup

---
+++ 01basicreact/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+    <h1>chai aur code | kumail abbas</h1>
+    <p>the quick brown fox jumps over a lazy dog</p>
+  </React.StrictMode>
+);
+
+
---
+++ 01vitereact/src/main.jsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+      <h1>kumail</h1>
+<p>kumail</p>
+  </StrictMode>,
+)
